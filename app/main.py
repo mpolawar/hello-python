25b2e5be839f753602c1a46abf4b41fd43c44e7b
mpolawar <37649735+mpolawar@users.noreply.github.com>
2021-06-16 12:39:51 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello it's mpolawar branch1 says Python to test the new workflow with new repo test webhooks!"
+    return "Hello it's mpolawar branch1 to test initial flow"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
