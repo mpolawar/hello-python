fac1ce73d7a5d8b84cd1cc5956799d54a467c59f
mpolawar <37649735+mpolawar@users.noreply.github.com>
2021-06-17 09:27:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello it's XLR/XLD/K8 deployment test"
+    return "Hello it's XLR/XLD/K8 deployment test for version 1.0.0"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
