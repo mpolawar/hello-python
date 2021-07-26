b1034d9fc7119be35dc02b330eae5c9c6bb989e0
mpolawar <37649735+mpolawar@users.noreply.github.com>
2021-07-26 11:31:23 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #Normal nginx app to display welcome message
 @app.route("/")
 def hello():
-    return "Hello it's XLR/XLD/K8 deployment with ingress host v 1.0"
+    return "Hello it's application deployment demo on K8 using DAI Release/Deploy:  Version 1.0"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
