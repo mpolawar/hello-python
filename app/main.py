2f8ddf4e93aff872d27decaba409779380f89896
mpolawar <37649735+mpolawar@users.noreply.github.com>
2021-07-26 10:05:11 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-#Normal nginx app display
+#Normal nginx app to display welcome message
 @app.route("/")
 def hello():
     return "Hello it's XLR/XLD/K8 deployment with ingress host v 1.0"
